03139bb79160fe76bfe0e775921d21f8b3975b43
jpha <josepabloha@live.com.mx>
2025-02-24 15:50:21 -0600

    try_solve

--- Web/Reportes/Teoria/T5/assets/IK/Try2/robot_check.py
+++ Web/Reportes/Teoria/T5/assets/IK/Try2/robot_check.py
@@ -24,10 +24,10 @@ robot = rtb.DHRobot(
     [
         rtb.RevoluteDH( alpha=np.pi/2,      a=0,    d=a1,       offset=0),
         rtb.RevoluteDH( alpha=0,            a=-a2,   d=0,        offset=-np.pi/2),
-        rtb.RevoluteDH( alpha=0,            a=-a3,   d=0,        offset=0),
-        rtb.RevoluteDH( alpha=np.pi/2,      a=0,    d=a4,    offset=-np.pi/2),
-        rtb.RevoluteDH( alpha=-np.pi/2,     a=0,    d=a5,        offset=0),
-        rtb.RevoluteDH( alpha=0,            a=0,    d=a6,    offset=np.pi)
+        rtb.RevoluteDH( alpha=0,            a=-a3,   d=0,        offset=0)
+        # rtb.RevoluteDH( alpha=np.pi/2,      a=0,    d=a4,    offset=-np.pi/2),
+        # rtb.RevoluteDH( alpha=-np.pi/2,     a=0,    d=a5,        offset=0),
+        # rtb.RevoluteDH( alpha=0,            a=0,    d=a6,    offset=np.pi)
     ],
     name=name
 )
@@ -40,7 +40,7 @@ print(robot)
 # Define the displacement values for each revolute joint
 t_values = [0, 0, 0, 0, 0, 0]
 d_values = [0, 0, 0, 0, 0, 0]  # Example: [X, Y, Z] displacements in meters
-q_values = [t_values[0], t_values[1], t_values[2], t_values[3], t_values[4], t_values[5]]
+q_values = [t_values[0], t_values[1], t_values[2]]
 
 print("Matriz de transformación (Directa):")
 T = robot.fkine(q_values)  # Forward kinematics
@@ -52,7 +52,7 @@ plt.savefig(f"Web/Reportes/Teoria/T5/assets/IK/Try2/FK {name}.png", dpi=600, bbo
 
 # Inverse kinematics ------------------------------------------------------
 #Definir la pose deseada del efector final (posición y orientación)
-Td = SE3(0.5, 0.2, 0.3) * SE3.RPY([0, np.pi/2, 0], order="xyz")
+Td = SE3(0.15, 0.15, 0.3) * SE3.RPY([0, np.pi/2, 0], order="xyz")
 
 print("Matriz de transformación (Inversa):")
 print(Td)
@@ -92,113 +92,42 @@ print("FK con GN:\n", robot.fkine(sol_GN.q))
 print("FK con NR:\n", robot.fkine(sol_NR.q))
 
 
-
-
 # Inverse by Geometry --------------------------------------------------------------
 # Definir los valores numéricos para la posición deseada del efector final
-x, y, z = 0.5, 0.2, 0.3
+x, y, z = 0.15, 0.015, 0.15
 
 # Definir los valores de los parámetros DH del UR5e
 d1, d2, d3, d4, d5, d6 = a1, a2, a3, a4, a5, a6
 
-#Conociendo Th5
-th5 = np.deg2rad(30)
-
 # Cálculo de los parámetros intermedios
 z1 = z - d1
-
-n2= d6 * np.cos(th5)
-n1 = d6 * np.sin(th5)
-n3 = d4 - n2
-
 hxy = np.sqrt(x**2 + y**2)
-h2 = np.sqrt(hxy**2 -n3**2)
+hxyz = np.sqrt(hxy**2 + z1**2)
 
-hxyz = np.sqrt(h2**2 + z1**2)
-
-phi3 = np.arctan2(y, x)
-
-
-ho3_o5 = np.sqrt(d5**2 - n1**2)
+phi3 = np.arctan2(z1, hxy)
+ho3_o5 = d5 / np.cos(phi3)
 ho1_o3 = hxyz - ho3_o5
 
-phi1 = np.arccos(h2/hxy)
-phi2 = np.arccos(-(d3**2 - (d2**2) - (ho1_o3**2)) / (2 * d2 * ho1_o3))
-phi4 = np.arctan(z1/h2)
-phi5 = np.arccos(-(ho1_o3**2 - (d2**2) - (d3**2)) / (2 * d2 * d3))
+n1 = np.sqrt(ho3_o5**2 - d5**2)
+n2 = np.sqrt(d6**2 - n1**2)
+
+phi1 = np.arccos(-(d3**2 - (d2**2) - (ho1_o3**2)) / (2 * d2 * ho1_o3))
+phi2 = np.arccos(-(ho1_o3**2 - (d2**2) - (d3**2)) / (2 * d2 * d3))
+phi4 = np.deg2rad(180) - phi1 - phi2
+phi5 = np.arctan(y/x)
+phi6 = np.arcsin((d4 - n2) / hxy)
 
 # Cálculo de los ángulos articulares
-th1 = phi3-phi1
-th2 = phi4-phi2
-th3 = np.deg2rad(180) - phi5
-th4 = (np.deg2rad(180) - phi2 -phi5 + phi4)
-th5 = th5
-th6 = np.deg2rad(0) 
+th1 = phi5 - phi6
+th2 = phi3 - phi1
+th3 = np.deg2rad(180) - phi2
+th4 = phi3 + phi4
+th5 = np.arcsin(n1 / d6)
+th6 = np.deg2rad(0)
 
 # Validación con Cinemática Directa
-q_numeric = [th1, th2, th3, th4, th5, th6]
-T_numeric = robot.fkine(q_numeric)
-
-# Mostrar los resultados
-print("\n--- Inverse by Geometry ---")
-print(np.rad2deg(q_numeric))
-
-
-print("\n--- Validación con FK (Numérica) ---")
-print(T_numeric)
-
-# Graficar la postura obtenida
-robot.plot(q_numeric, block=Status_block, jointaxes=True, eeframe=True, jointlabels=True)
-plt.savefig(f"Web/Reportes/Teoria/T5/assets/IK/Try2/FK_numpy_{name}.png", dpi=600, bbox_inches='tight', pad_inches=0.1)
-
-
-# Inverse by Geometry 3--------------------------------------------------------------
-
-# Definir los valores de los parámetros DH del UR5e
-d1, d2, d3, d4, d5, d6 = a1, a2, a3, a4, a5, a6
-
-#Conociendo Th5
-th5 = np.deg2rad(30)
-
-# Cálculo de los parámetros intermedios
-z1 = z - d1
-
-n2= d6 * np.cos(th5)
-n1 = d6 * np.sin(th5)
-n3 = d4 - n2
-
-hxy = np.sqrt(x**2 + y**2)
-h2 = np.sqrt(hxy**2 -n3**2)
-
-hxyz = np.sqrt(h2**2 + z1**2)
-
-phi3 = np.arctan2(y, x)
-
-
-ho3_o5 = np.sqrt(d5**2 - n1**2)
-
-h3 = np.sqrt(hxy**2 - n1**2)
-
-ho1_o3 = h3-d5
-
-phi1 = np.arccos(h2/hxy)
-phi2 = np.arccos(-(d3**2 - (d2**2) - (ho1_o3**2)) / (2 * d2 * ho1_o3))
-phi4 = np.arccos(-(ho1_o3**2 - (d2**2) - (d3**2)) / (2 * d2 * d3))
-phi5 = np.arctan(z1/h2)
-phi6 = np.deg2rad(180) - phi2 - phi4
-phi7 = np.arccos(h3/hxyz)
-
-
-# Cálculo de los ángulos articulares
-th1 = phi3-phi1
-th2 = phi4-phi2
-th3 = np.deg2rad(180) - phi5
-th4 = (np.deg2rad(180) - phi2 -phi5 + phi4)
-th5 = th5
-th6 = np.deg2rad(0) 
-
-# Validación con Cinemática Directa
-q_numeric = [th1, th2, th3, th4, th5, th6]
+#q_numeric = [th1, th2, th3, th4, th5, th6]
+q_numeric = [th1, th2, th3]
 T_numeric = robot.fkine(q_numeric)
 
 # Mostrar los resultados
