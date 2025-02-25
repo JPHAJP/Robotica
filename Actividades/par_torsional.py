69a96aaa973e1e46130b22c86ce3f8672bdc2766
jpha <josepabloha@live.com.mx>
2025-02-24 18:22:42 -0600

    cambio reporte lab3

--- Actividades/par_torsional.py
+++ Actividades/par_torsional.py
@@ -17,13 +17,8 @@ print(np.round(T_max1/10**6,2), " MPa")
 r=(46/2)*10**-3
 J=1/2*np.pi*r**4
 print(J)
-T=400
+T=400+300
 c=r
 
 T_max2=(T*c)/J
-print(np.round(T_max2/10**6,2), " MPa")
-T_max2=T_max1-T_max2
-
-#Resultado en MPa
-print("El esfuerzo maximo es:")
 print(np.round(T_max2/10**6,2), " MPa")
